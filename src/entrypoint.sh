2c7d9e0880742507c435f255411915ea046933e6
Dan Walkes <danwalkes@trellis-logic.com>
2021-06-26 16:21:12 -0600

    Pass environment into su session
    For SSH keys, to allow defining through environment vars.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -21,13 +21,18 @@ while getopts ":i:g:" opt; do
 done
 shift $((OPTIND -1))
 workdir=$(pwd)
+whitelist_env="--whitelist-environment=SSH_PRIVATE_KEY,SSH_PRIVATE_KEY_BASE64"
 if [ ! -z "${uid}" ]; then
     echo "Remapping uid ${uid} to autotest-admin user"
     usermod -u $uid autotest-admin
     groupmod -g $gid autotest-admin
     # Run as the autotest-admin with newly mapped user and group
     # permissions.  We won't return from this step
-    exec su --login autotest-admin /bin/bash -c "cd ${workdir} && $@"
+    exec su  ${whitelist_env} \
+        --login autotest-admin \
+        /bin/bash -c "cd ${workdir} && $@"
 fi
 # If we don't specify a user and group to map to, just run as root
-exec su --login root /bin/bash -c "cd ${workdir} && $@"
+exec su  ${whitelist_env} \
+        --login root \
+        /bin/bash -c "cd ${workdir} && $@"
