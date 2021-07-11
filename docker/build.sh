54fe4b10abb5303d107f2abc924f5e897f5f04c3
Dan Walkes <danwalkes@trellis-logic.com>
2021-07-10 18:18:14 -0600

    Changes for assignment 6 and 7

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 set -e
 pushd $(dirname $0)/..
 #See https://devops.stackexchange.com/a/4450
-for number in $(seq 6) 4-buildroot 5-buildroot 6-yocto; do
+for number in $(seq 7) 4-buildroot 5-buildroot 6-yocto 7-buildroot; do
     docker build -f docker/Dockerfile --target assignment${number} -t cuaesd/aesd-autotest:assignment${number} src
 done
 docker tag cuaesd/aesd-autotest:assignment1 cuaesd/aesd-autotest:unit-test
--- docker/push-all.sh
+++ docker/push-all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
-for number in $(seq 6); do
+for number in $(seq 7); do
     docker push cuaesd/aesd-autotest:assignment${number}
 done
 docker push cuaesd/aesd-autotest:assignment4-buildroot
 docker push cuaesd/aesd-autotest:assignment5-buildroot
+docker push cuaesd/aesd-autotest:assignment7-buildroot
 docker push cuaesd/aesd-autotest:assignment6-yocto
 docker push cuaesd/aesd-autotest:unit-test
