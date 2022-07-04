4173f83154c035a27450a299f0c78864468ca3c9
Dan Walkes <danwalkes@trellis-logic.com>
2022-07-04 13:55:30 -0600

    Fix yocto docker build
    Use crops/poky-container at [1] to support yocto builds with docker
    
    1: https://github.com/crops/poky-container

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,9 @@
 set -e
 pushd $(dirname $0)/..
 #See https://devops.stackexchange.com/a/4450
-for number in $(seq 7) 4-buildroot 5-buildroot 6-yocto 7-buildroot; do
+for number in $(seq 7) 4-buildroot 5-buildroot 7-buildroot; do
     docker build -f docker/Dockerfile --target assignment${number} -t cuaesd/aesd-autotest:assignment${number} src
 done
 docker tag cuaesd/aesd-autotest:assignment1 cuaesd/aesd-autotest:unit-test
+
+docker build -f docker/Dockerfile-yocto --target assignment6-yocto -t cuaesd/aesd-autotest:assignment6-yocto src
