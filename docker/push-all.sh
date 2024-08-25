f1477adc6898304372c2496394ce762f60e5eace
Dan Walkes <danwalkes@trellis-logic.com>
2024-08-25 15:28:29 -0600

    Update to Ubuntu 22.04
    * Switch to ubuntu 22.04 as build host from 20.04
    * Use docker buildx to ultimately support multi-platform builds.
    * Update gcc arm compiler for use with 22.04

--- docker/push-all.sh
+++ docker/push-all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
+TAGPREFIX="24-"
 for number in $(seq 7); do
-    docker push cuaesd/aesd-autotest:assignment${number}
+    docker push cuaesd/aesd-autotest:${TAGPREFIX}assignment${number}
 done
-docker push cuaesd/aesd-autotest:assignment4-buildroot
-docker push cuaesd/aesd-autotest:assignment5-buildroot
-docker push cuaesd/aesd-autotest:assignment7-buildroot
-docker push cuaesd/aesd-autotest:assignment6-yocto
-docker push cuaesd/aesd-autotest:unit-test
+docker push cuaesd/aesd-autotest:${TAGPREFIX}assignment4-buildroot
+docker push cuaesd/aesd-autotest:${TAGPREFIX}assignment5-buildroot
+docker push cuaesd/aesd-autotest:${TAGPREFIX}assignment7-buildroot
+docker push cuaesd/aesd-autotest:${TAGPREFIX}assignment6-yocto
+docker push cuaesd/aesd-autotest:${TAGPREFIX}unit-test
